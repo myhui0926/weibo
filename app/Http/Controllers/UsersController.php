65a7595a0043366fdcabb51866c1e9f9be9c052a
SongChaoHui <992414441@qq.com>
2019-02-02 17:11:46 +0000

    用户管理及权限设定

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,12 +8,29 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['show','create','store','index']
+        ]);
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
+    public function index(){
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
     public function create(){
         return view('users.create');
     }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
     public function store(Request $request){
         $this->validate($request,[
             'name'=>'required|max:50',
@@ -31,6 +48,31 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){
+        $this->authorize('update',$user);
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','个人资料更新成功');
+        return redirect()->route('users.show',$user->id);
+    }
+
+    public function destroy(User $user){
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return redirect()->back();
     }
 }
